2e890d5392c8d83111615780dafc6c0a91f8cc9c
Ogun Karaoglu <ogunkaraoglu62@gmail.com>
2024-01-10 15:23:05 +0300

    footer and clients section

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { card } from '../assets'
+import styles, { layout } from '../style.js'
+import Button from './Button.jsx'
 
-const CardDeal = () => {
-  return <div>CardDeal</div>
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi
+        tenetur nulla consequatur labore odio consectetur veniam doloribus nihil
+        illo cumque.
+      </p>
+      <Button styles="mt-10" />
+    </div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+)
 
 export default CardDeal
--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { clients } from '../constants'
+import styles from '../style.js'
 
-const Clients = () => {
-  return <div>Clients</div>
-}
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+        >
+          <img
+            src={client.logo}
+            alt="client"
+            className="sm:w-[192px] object-contain w-[100px]"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+)
 
 export default Clients
